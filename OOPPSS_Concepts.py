2a91692561c653affff547e71497379e17c7d85b
prateek <prateekmitel@yahoo.com>
2021-06-20 20:02:00 +0000

    functions in python practice

--- OOPPSS_Concepts.py
+++ OOPPSS_Concepts.py
@@ -3,3 +3,18 @@ class Student:
     #Variable
     #Method
 print(Student.__doc__)
+
+'''
+Object Is The Physical Implementation of the Class
+Objects are stored in reference variable
+'''
+class Student:
+    def input(self):
+        roll=int(input("Enter the Roll Number:"))
+        name=input("Enter the name of the student:")
+        standard=int(input("Enter the standard in which student is studying:"))
+        print(roll)
+        print(name)
+        print(standard)
+st1=Student()
+st1.input()
