9a01792d3377dd555f305e84f1b64269dfdb3a9e
prateek <prateekmitel@yahoo.com>
2021-06-20 16:09:58 +0000

    list related exercises and code

---
+++ tuplles.py
@@ -0,0 +1,55 @@
+'''
+TUPLES are IMMUTABLE(C--->Creates Yes
+                     R---> Read Yes
+                     U--->Update No
+                     D--->Delete Yes)
+'''
+
+tuple1=(1,2,3,4,5,6,7,'Prateek',12.7,'!','@')
+print(tuple1)
+#We can have a list inside a tuple , but we can not modify any element of the list as this would break clause of tuple being
+#immutable
+
+# n=int(input())
+# print(n*2,"\n")
+# str1=input()
+# print(str1)
+
+
+'''
+Tuple PACKING
+'''
+a=1.5,2,3,'PRATEEK'
+print(type(a))
+print(a)
+
+
+'''
+TUPLE UNPACKING
+'''
+b,c,d,e=a
+print(b,c,d,e)
+
+'''
+Conversion of a Tuple into a List
+'''
+
+tuple2=1,2,3,4.7,'PRATEEK'
+print(type(tuple2))
+print(tuple2)
+list2=list(tuple2)
+print(type(list2))
+print(list2)
+
+
+'''
+Conversion of a List into a Tuple
+'''
+
+
+list3=[1,2,3,4,5,'PRATEEK','!','@','#']
+print(type(list3))
+print(list3)
+tuple3=tuple(list3)
+print(type(tuple3))
+print(tuple3)
