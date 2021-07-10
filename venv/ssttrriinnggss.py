e5e11cb92f8b4d8c5db6d145b7036b07cf061cac
prateek <prateekmitel@yahoo.com>
2021-07-10 12:01:50 +0000

    strings concept

---
+++ venv/ssttrriinnggss.py
@@ -0,0 +1,19 @@
+a="hello , world , how are you doing?!"
+print(a.upper())
+
+b="HELLO , WORLD , HOW ARE YOU DOING?!"
+print(b.lower())
+
+'''
+The strip() method removes any whitespace from the beginning or the end
+'''
+c="     hi hello how are you doing sir!!!     "
+print(c)
+print(c.strip())
+
+'''
+The replace() method replaces a string with another string
+'''
+d="Hello , World"
+print(d.replace("Hello","Mellow"))
+
