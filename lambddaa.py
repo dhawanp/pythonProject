2cf0b4945a47598e2442e52d0f90fb9f81b760fa
prateek <prateekmitel@yahoo.com>
2021-08-08 21:25:22 +0000

    lammbdaa

---
+++ lambddaa.py
@@ -0,0 +1,6 @@
+'''A lambda function is a small anonymous function.
+
+A lambda function can take any number of arguments, but can only have one expression.'''
+
+x = lambda a, b : a * b
+print(x(5, 6))
